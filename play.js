4404ee63daa9065d93b55e9bb4cd83c5d91e0795
RajMazing <caseyraj92@gmail.com>
2021-12-02 00:12:24 -0600

    added handleUserInput and setupInput functions with exit method

--- play.js
+++ play.js
@@ -7,3 +7,39 @@ const { connect } = require("./client")
 console.log("Connecting ...");
 connect();
 
+// setup interface to handle user input from stdin
+
+
+
+const setupInput = function (key) {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function (data) {
+  // your code here
+  process.stdin.on('data', (key) => {
+  if (key === '\u0003') {
+    console.log("Terminate")
+    process.exit();
+   
+  }
+});
+}
+setupInput();
+
+
+module.exports = {setupInput};
+
+
+
+
+
+
+
+
+
